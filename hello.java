8054261de808b2c99b677000cef68ab2c53f0804
nithish <nithish162965@gmail.com>
2024-07-26 05:32:47 +0530

    nithish removed

--- hello.java
+++ hello.java
@@ -3,6 +3,6 @@ package python;
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("nithish");
+        
     }
 }
